acc00f63164ff33fb5573c2441e956c6f187da51
edith <perezedith173@gmail.com>
2024-05-27 17:57:08 -0600

    Fix scroll-up

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,20 +3,16 @@ const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
 
-
 // Scroll to top selection
-const scrollUp = document.querySelector("#Scroll-up");
-
+const scrollUp = document.querySelector("#scroll-up");
 
 // Select nav links
 const navLink = document.querySelectorAll(".nav-link");
-
 
 // Hamburger menu function
 burger.addEventListener("click", () => {
   ul.classList.toggle("show");
 });
-
 
 // Close hamburger menu when a link is clicked
 navLink.forEach((link) =>
@@ -25,15 +21,11 @@ navLink.forEach((link) =>
   })
 );
 
-//scroll top selection
-//const scrollUp = document.querySelector('#Scroll-up');
-
-//funciontionaly scroll
-//scrollUp.addEventListener('click', () =>
-   // {window.scrollTo({
-     //   top: 0,
-   //     left: 0,
- //       behavior:'smooth',
- //   });
-//    }
-//);
+// scroll to top functionality
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
